cbbcc38a8db0f4d3309b974f6607da0737a86ecd
Shun Yanaura <metroplexity@gmail.com>
2022-12-23 23:51:41 +0900

    feat: add literal lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type TokenKind int8
 
@@ -8,6 +10,25 @@ const (
 	TokUnknown TokenKind = iota - 1
 	TokNull    TokenKind = iota
 	TokString
+	TokBool
+	TokStar
+	TokPlus
+	TokTilde
+	TokCaret
+	TokComma
+	TokDollar
+	TokGreater
+	TokSemicolon
+	TokSlashDash
+	TokDoublePipe
+	TokEqual
+	TokNotEqual
+	TokOpenPar
+	TokClosePar
+	TokOpenBrace
+	TokCloseBrace
+	TokOpenBracket
+	TokCloseBracket
 )
 
 type Token struct {
@@ -30,8 +51,12 @@ type Lexer struct {
 	stack   []Token
 }
 
-func (l *Lexer) EOF() bool {
-	return l.current >= len(l.source)
+func (l *Lexer) eof() bool {
+	return l.eofAt(0)
+}
+
+func (l *Lexer) eofAt(offset int) bool {
+	return l.current+offset >= len(l.source)
 }
 
 func (l *Lexer) addToken(kind TokenKind, start, until int) {
@@ -47,7 +72,7 @@ func (l *Lexer) consume(amount int) {
 }
 
 func (l *Lexer) peek() rune {
-	if l.EOF() {
+	if l.eof() {
 		return 0
 	}
 
@@ -55,7 +80,22 @@ func (l *Lexer) peek() rune {
 }
 
 func (l *Lexer) until(until int) string {
+	if l.eofAt(until - 1) {
+		return l.source[l.current:]
+	}
+
 	return l.source[l.current : l.current+until]
+}
+
+func (l *Lexer) literal(lit string) bool {
+	fmt.Println("literal: ", lit)
+	if l.until(len(lit)) == lit {
+		l.consume(len(lit))
+
+		return true
+	}
+
+	return false
 }
 
 func (l *Lexer) TokenizeString(raw bool) {
@@ -67,25 +107,61 @@ func (l *Lexer) TokenizeString(raw bool) {
 	}
 }
 
-var whiteSpaces = []rune{
-	'\u0009',
-	'\u0020',
-	'\u00A0',
-	'\u1680',
-	'\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200A',
-	'\u202F',
-	'\u205F',
-	'\u3000',
-}
+var (
+	digits         = []rune("0123456789")
+	nonIdentifiers = []rune{'\\', '/', '(', ')', '{', '}', '<', '>', ';', '[', ']', '=', ',', '"'}
+	whiteSpaces    = []rune{
+		'\u0009',
+		'\u0020',
+		'\u00A0',
+		'\u1680',
+		'\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006', '\u2007', '\u2008', '\u2009', '\u200A',
+		'\u202F',
+		'\u205F',
+		'\u3000',
+	}
+	newLines = []string{
+		"\r\n",
+		"\r",
+		"\n",
+		"\u0085",
+		"\f",
+		"\u2028",
+		"\u2029",
+	}
+	literals = map[string]TokenKind{
+		"null":  TokNull,
+		"true":  TokBool,
+		"false": TokBool,
+		"*":     TokStar,
+		"+":     TokPlus,
+		"~":     TokTilde,
+		"^":     TokCaret,
+		",":     TokComma,
+		"$":     TokDollar,
+		">":     TokGreater,
+		";":     TokSemicolon,
+		"/-":    TokSlashDash,
+		"||":    TokDoublePipe,
+		"==":    TokEqual,
+		"!=":    TokNotEqual,
+		"(":     TokOpenPar,
+		")":     TokClosePar,
+		"{":     TokOpenBrace,
+		"}":     TokCloseBrace,
+		"[":     TokOpenBracket,
+		"]":     TokCloseBracket,
+	}
+)
 
-var newLines = []string{
-	"\r\n",
-	"\r",
-	"\n",
-	"\u0085",
-	"\f",
-	"\u2028",
-	"\u2029",
+func isNonIdentifier(r rune) bool {
+	for _, ni := range nonIdentifiers {
+		if ni == r {
+			return true
+		}
+	}
+
+	return false
 }
 
 func isWhiteSpace(r rune) bool {
@@ -98,15 +174,16 @@ func isWhiteSpace(r rune) bool {
 	return false
 }
 
-func lexWhiteSpace(l *Lexer) bool {
-	for !l.EOF() && isWhiteSpace([]rune(l.source)[l.current]) {
+func (l *Lexer) tokenizeWhiteSpace() bool {
+	fmt.Printf("source: %s, current: %d\n", l.source, l.current)
+	for !l.eof() && isWhiteSpace([]rune(l.source)[l.current]) {
 		l.consume(1)
 	}
 
 	return false
 }
 
-func lexNewLine(l *Lexer) bool {
+func (l *Lexer) tokenizeNewLine() bool {
 	for _, nl := range newLines {
 		if l.source[l.current:l.current+len(nl)] == nl {
 			l.consume(len(nl))
@@ -117,8 +194,9 @@ func lexNewLine(l *Lexer) bool {
 	return false
 }
 
-func (l *Lexer) lexString() /*(l *Lexer)*/ bool {
+func (l *Lexer) tokenizeString() /*(l *Lexer)*/ bool {
 	before := l.current
+
 	if l.peek() != '"' {
 		l.current = before
 		return false
@@ -128,14 +206,15 @@ func (l *Lexer) lexString() /*(l *Lexer)*/ bool {
 
 	terminated := false
 
-	for !l.EOF() {
+L:
+	for !l.eof() {
 		switch l.peek() {
 		case '\\':
 			// TODO: escape sequences
 		case '"':
 			l.consume(1)
 			terminated = true
-			break
+			break L
 		default:
 			l.consume(1)
 		}
@@ -147,24 +226,60 @@ func (l *Lexer) lexString() /*(l *Lexer)*/ bool {
 
 	l.addToken(TokString, before, l.current)
 
+	return true
+}
+
+func (l *Lexer) tokenizeLiteral() bool {
+	if l.eof() {
+		return false
+	}
+
+	before := l.current
+
+	for lit, kind := range literals {
+		if l.eof() {
+			break
+		}
+		if l.literal(lit) {
+			l.addToken(kind, before, l.current)
+			return true
+		}
+	}
+
 	return false
+}
+
+func (l *Lexer) tokenizeIdentifier() bool {
+	if l.eof() || isNonIdentifier(l.peek()) {
+		return false
+	}
+
+	before := l.current
+
+	for !l.eof() && !isWhiteSpace(l.peek()) {
+		l.consume(1)
+	}
+
+	l.addToken(TokUnknown, before, l.current)
+
+	return true
 }
 
 type lexFn func(l *Lexer) bool
 
 var choices = []lexFn{
-	//lexWhiteSpace,
-	//lexNewLine,
-	(*Lexer).lexString,
+	(*Lexer).tokenizeWhiteSpace,
+	//(*Lexer).tokenizeNewLine,
+	(*Lexer).tokenizeString,
+	(*Lexer).tokenizeLiteral,
 }
 
 func (l *Lexer) Scan() error {
 	var matchedSomehow bool
-	for !l.EOF() {
+	for !l.eof() {
 		for _, choice := range choices {
 			if choice(l) {
 				matchedSomehow = true
-				break
 			}
 		}
 		if !matchedSomehow {
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -6,14 +6,90 @@ import (
 )
 
 func TestLexer(t *testing.T) {
-	t.Run("lexString", func(t *testing.T) {
-		input := `"hello"`
-		l := NewLexer(input)
-		l.lexString()
-		assert.Equal(t, l.stack, []Token{{kind: TokString, lexeme: `"hello"`, start: 0}})
+	t.Parallel()
+
+	t.Run("tokenizeString", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name  string
+			input string
+			want  []Token
+		}{
+			{
+				name:  "simple",
+				input: `"hello"`,
+				want:  []Token{{kind: TokString, lexeme: `"hello"`, start: 0}},
+			},
+			{
+				name:  "sequence",
+				input: `"hello" "world"`,
+				want:  []Token{{kind: TokString, lexeme: `"hello"`, start: 0}},
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				l := NewLexer(tt.input)
+				l.tokenizeString()
+				assert.Equal(t, tt.want, l.stack)
+			})
+		}
+	})
+
+	t.Run("tokenizeLiteral", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name  string
+			input string
+			want  []Token
+		}{
+			{
+				name:  "null",
+				input: `null`,
+				want:  []Token{{kind: TokNull, lexeme: `null`, start: 0}},
+			},
+			{
+				name:  "false",
+				input: `false`,
+				want:  []Token{{kind: TokBool, lexeme: `false`, start: 0}},
+			},
+			{
+				name:  "true",
+				input: `true`,
+				want:  []Token{{kind: TokBool, lexeme: `true`, start: 0}},
+			},
+			{
+				name:  "equal",
+				input: `==`,
+				want:  []Token{{kind: TokEqual, lexeme: `==`, start: 0}},
+			},
+			{
+				name:  "not equal",
+				input: `!=`,
+				want:  []Token{{kind: TokNotEqual, lexeme: `!=`, start: 0}},
+			},
+		}
+
+		for _, tt := range tests {
+			tt := tt
+
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
+				l := NewLexer(tt.input)
+				l.tokenizeLiteral()
+				assert.Equal(t, tt.want, l.stack)
+			})
+		}
 	})
 
 	t.Run("Scan", func(t *testing.T) {
+		t.Parallel()
+
 		type fields struct {
 			source  string
 			current int
@@ -37,14 +113,37 @@ func TestLexer(t *testing.T) {
 					},
 				},
 			},
+			{
+				name: "sequential strings",
+				fields: fields{
+					source:  `"hello" "world"`,
+					current: 15,
+					stack: []Token{
+						{
+							kind:   TokString,
+							start:  0,
+							lexeme: `"hello"`,
+						},
+						{
+							kind:   TokString,
+							start:  8,
+							lexeme: `"world"`,
+						},
+					},
+				},
+			},
 		}
 
 		for _, tt := range tests {
+			tt := tt
 			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+
 				l := &Lexer{
 					source: tt.fields.source,
 				}
-				l.Scan()
+				err := l.Scan()
+				assert.NoError(t, err)
 				assert.Equal(t, tt.fields.current, l.current)
 				assert.Equal(t, tt.fields.stack, l.stack)
 			})
